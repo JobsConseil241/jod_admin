48f94bcac8692f52ea8689e647933584d02f0e4b
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-11-28 22:06:48 +0100

    User Managment Develop 15

--- resources/views/back/user/role.blade.php
+++ resources/views/back/user/role.blade.php
@@ -3,6 +3,12 @@
 @push('styles')
     <!--datatable css-->
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.tailwindcss.css" />
+
+    <style>
+        .col-start-2 {
+            grid-column-start: 3 !important;
+        }
+    </style>
 @endpush
 
 @inject('Lang', 'App\Services\LanguageService')
@@ -35,6 +41,10 @@
             <div class="box">
                 <div class="box-header">
                     <h5 class="box-title">Liste des Rôles</h5>
+                    <button type="button" class="hs-dropdown-toggle ti-btn ti-btn-primary justify-end"
+                        data-hs-overlay="#hs-basic-modal">
+                        Ajouter un Rôle
+                    </button>
                 </div>
                 <div class="box-body">
                     <table id="data" class="table table-bordered dt-responsive nowrap table-striped align-middle"
@@ -86,7 +96,7 @@
         </div>
     </div>
 
-    <div id="hs-basic-modal" class="hs-overlay ti-modal open" aria-overlay="true" tabindex="-1">
+    <div id="hs-basic-modal" class="hs-overlay ti-modal hidden">
         <div class="ti-modal-box">
             <div class="ti-modal-content">
                 <form action="{{ url('backend/role') }}" method="POST">
@@ -108,8 +118,8 @@
                     </div>
                     <div class="ti-modal-body">
                         <div class="mb-3">
-                            <label for="user_type_id" class="col-form-label">{{ $Lang->trans('user_type') }}</label>
-                            <select id="selectOne" class="form-control" name="user_type_id">
+                            <label for="hs-select-label" class="ti-form-select-label">Type d'utilisateur</label>
+                            <select name="user_type_id" id="hs-select-label" class="ti-form-select">
                                 @foreach ($types as $type)
                                     <option value="{{ $type->id }}">{{ $type->name }}</option>
                                 @endforeach
@@ -117,19 +127,19 @@
                         </div>
 
                         <div class="mb-3">
-                            <label for="name" class="col-form-label">{{ $Lang->trans('name') }}</label>
-                            <input type="text" class="form-control" name="name">
+                            <label for="input-label" class="ti-form-label">Nom</label>
+                            <input type="text" id="input-label" class="ti-form-input">
                         </div>
 
                         <div class="mb-3">
-                            <label for="name" class="col-form-label">{{ $Lang->trans('description') }}</label>
-                            <textarea class="form-control" name="description"></textarea>
+                            <label for="input-description" class="ti-form-label">Description</label>
+                            <textarea class="ti-form-input" rows="3" name="description"></textarea>
                         </div>
 
                         <!--begin::Permissions-->
                         <div class="fv-row">
                             <!--begin::Label-->
-                            <label class="fs-5 fw-bold form-label mb-2">{{ $Lang->trans('privileges') }}</label>
+                            <label for="input-label" class="ti-form-label">Privilèges</label>
                             <!--end::Label-->
                             <!--begin::Table wrapper-->
                             <div class="table-responsive">
@@ -141,29 +151,21 @@
                                             <!--begin::Table row-->
                                             <tr>
                                                 <!--begin::Label-->
-                                                <td class="text-gray-500">{{ $pr->name }}</td>
-                                                <!--end::Label-->
-                                                <!--begin::Options-->
-                                                <td>
-                                                    <!--begin::Wrapper-->
-                                                    <div class="d-flex">
-                                                        <!--begin::Checkbox-->
-                                                        <label
-                                                            class="form-check form-check-sm form-check-custom form-check-solid me-5 me-lg-20">
-                                                            <input class="form-check-input" type="checkbox"
-                                                                value="{{ $pr->id }}" name="privileges[]" />
-                                                        </label>
-                                                        <!--end::Checkbox-->
-
-                                                    </div>
-                                                    <!--end::Wrapper-->
+                                                <td class="text-gray-500">
+                                                    <label
+                                                        class="max-w-xs flex p-3 w-full bg-white border border-gray-200 rounded-sm text-sm focus:border-primary focus:ring-primary dark:bg-bgdark dark:border-white/10 dark:text-white/70">
+                                                        <input name="privileges[]" type="checkbox"
+                                                            value="{{ $pr->id }}"
+                                                            class="shrink-0 mt-0.5 border-gray-200 rounded text-primary pointer-events-none focus:ring-primary dark:bg-bgdark dark:border-white/10 dark:checked:bg-primary dark:checked:border-primary dark:focus:ring-offset-white/10"
+                                                            id="vertical-checkbox-checked-in-form">
+                                                        <span
+                                                            class="text-sm text-gray-500 ltr:ml-2 rtl:mr-2 dark:text-white/70">{{ $pr->name }}</span>
+                                                    </label>
                                                 </td>
-                                                <!--end::Options-->
+                                                <!--end::Label-->
                                             </tr>
                                             <!--end::Table row-->
                                         @endforeach
-
-
                                     </tbody>
                                     <!--end::Table body-->
                                 </table>
@@ -195,6 +197,45 @@
             </div>
         </div>
     </div>
+
+    <div id="cardModalView" class="hs-overlay ti-modal hidden">
+        <div class="ti-modal-box">
+            <div class="ti-modal-content">
+            </div>
+        </div>
+    </div>
+
+    {{-- <div class="ti-modal-header">
+        <h3 class="ti-modal-title">
+          Modal title
+        </h3>
+        <button type="button" class="hs-dropdown-toggle ti-modal-clode-btn"
+          data-hs-overlay="#hs-basic-modal">
+          <span class="sr-only">Close</span>
+          <svg class="w-3.5 h-3.5" width="8" height="8" viewBox="0 0 8 8" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M0.258206 1.00652C0.351976 0.912791 0.479126 0.860131 0.611706 0.860131C0.744296 0.860131 0.871447 0.912791 0.965207 1.00652L3.61171 3.65302L6.25822 1.00652C6.30432 0.958771 6.35952 0.920671 6.42052 0.894471C6.48152 0.868271 6.54712 0.854471 6.61352 0.853901C6.67992 0.853321 6.74572 0.865971 6.80722 0.891111C6.86862 0.916251 6.92442 0.953381 6.97142 1.00032C7.01832 1.04727 7.05552 1.1031 7.08062 1.16454C7.10572 1.22599 7.11842 1.29183 7.11782 1.35822C7.11722 1.42461 7.10342 1.49022 7.07722 1.55122C7.05102 1.61222 7.01292 1.6674 6.96522 1.71352L4.31871 4.36002L6.96522 7.00648C7.05632 7.10078 7.10672 7.22708 7.10552 7.35818C7.10442 7.48928 7.05182 7.61468 6.95912 7.70738C6.86642 7.80018 6.74102 7.85268 6.60992 7.85388C6.47882 7.85498 6.35252 7.80458 6.25822 7.71348L3.61171 5.06702L0.965207 7.71348C0.870907 7.80458 0.744606 7.85498 0.613506 7.85388C0.482406 7.85268 0.357007 7.80018 0.264297 7.70738C0.171597 7.61468 0.119017 7.48928 0.117877 7.35818C0.116737 7.22708 0.167126 7.10078 0.258206 7.00648L2.90471 4.36002L0.258206 1.71352C0.164476 1.61976 0.111816 1.4926 0.111816 1.36002C0.111816 1.22744 0.164476 1.10028 0.258206 1.00652Z"
+              fill="currentColor" />
+          </svg>
+        </button>
+      </div>
+      <div class="ti-modal-body">
+        <p class="mt-1 text-gray-800 dark:text-white/70">
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+        </p>
+      </div>
+      <div class="ti-modal-footer">
+        <button type="button"
+          class="hs-dropdown-toggle ti-btn ti-border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:ring-offset-white focus:ring-primary dark:bg-bgdark dark:hover:bg-black/20 dark:border-white/10 dark:text-white/70 dark:hover:text-white dark:focus:ring-offset-white/10"
+          data-hs-overlay="#hs-basic-modal">
+          Close
+        </button>
+        <a class="ti-btn ti-btn-primary"
+          href="javascript:void(0);">
+          Save changes
+        </a>
+      </div> --}}
 @endsection
 
 @push('scripts')
