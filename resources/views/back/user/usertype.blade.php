d98778047e2e71d436a77b8a4db4dd77a7eb4f47
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-11-28 04:12:45 +0100

    User Managment Develop 10

--- resources/views/back/user/usertype.blade.php
+++ resources/views/back/user/usertype.blade.php
@@ -65,7 +65,6 @@
 @push('scripts')
     <!--datatable js-->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="https://cdn.tailwindcss.com/"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.tailwindcss.js"></script>
     <script>
