1bd501980768641473bbeedb20ab3e3caedad5f4
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-11-28 22:53:35 +0100

    User Managment Develop 24

--- resources/views/back/user/privilege.blade.php
+++ resources/views/back/user/privilege.blade.php
@@ -3,46 +3,56 @@
 @push('styles')
     <!--datatable css-->
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.tailwindcss.css" />
+
+    <style>
+        .col-start-2 {
+            grid-column-start: 3 !important;
+        }
+    </style>
 @endpush
 
 @inject('Lang', 'App\Services\LanguageService')
 
 @section('content')
-    <!-- start page title -->
-    <div class="row">
-        <div class="col-12">
-            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h4 class="mb-sm-0">{{ $Lang->trans('privileges') }}</h4>
-
-                <div class="page-title-right">
-                    <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">{{ $Lang->trans('management_user') }}</a>
-                        </li>
-                        <li class="breadcrumb-item active">{{ $Lang->trans('privileges') }}</li>
-                    </ol>
-                </div>
-
-            </div>
+    <!-- Page Header -->
+    <div class="block justify-between page-header md:flex">
+        <div>
+            <h3 class="text-gray-700 hover:text-gray-900 dark:text-white dark:hover:text-white text-2xl font-medium">
+                Privilèges</h3>
         </div>
+        <ol class="flex items-center whitespace-nowrap min-w-0">
+            <li class="text-sm">
+                <a class="flex items-center font-semibold text-primary hover:text-primary dark:text-primary truncate"
+                    href="{{ route('dashboard') }}">
+                    Tableau de Bord
+                    <i
+                        class="ti ti-chevrons-right flex-shrink-0 mx-3 overflow-visible text-gray-300 dark:text-gray-300 rtl:rotate-180"></i>
+                </a>
+            </li>
+            <li class="text-sm text-gray-500 hover:text-primary dark:text-white/70 " aria-current="page">
+                Privilèges
+            </li>
+        </ol>
     </div>
-    <!-- end page title -->
+    <!-- Page Header Close -->
 
     <!--begin::Container-->
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">{{ $Lang->trans('list_privileges') }}</h5>
+
+
+    <div class="grid grid-cols-12 gap-6">
+        <div class="col-span-12">
+            <div class="box">
+                <div class="box-header">
+                    <h5 class="box-title">Liste des Privilèges</h5>
                 </div>
-                <div class="card-body">
-                    <table id="data" class="table table-bordered dt-responsive nowrap table-striped align-middle"
-                        style="width:100%">
+                <div class="box-body">
+                    <table id="data" class="display" style="width:100%">
                         <thead>
                             <tr>
-                                <th data-ordering="false">{{ $Lang->trans('id') }}</th>
-                                <th>{{ $Lang->trans('user_type') }}</th>
-                                <th>{{ $Lang->trans('name') }}</th>
-                                <th>{{ $Lang->trans('description') }}</th>
+                                <th data-ordering="false">ID</th>
+                                <th>Types d'Utilisateurs</th>
+                                <th>Nom</th>
+                                <th>Description</th>
 
                             </tr>
                         </thead>
@@ -59,16 +69,16 @@
                     </table>
                 </div>
             </div>
-        </div><!--end col-->
-    </div><!--end row-->
+        </div>
+    </div>
     <!--end::Container-->
 @endsection
 
 @push('scripts')
+    <!--datatable js-->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.tailwindcss.js"></script>
-
     <script>
         $(document).ready(function() {
             $("#data").DataTable({
@@ -76,35 +86,6 @@
                     url: "https://cdn.datatables.net/plug-ins/1.10.25/i18n/French.json"
                 },
                 searching: true
-            })
-        }), document.addEventListener("DOMContentLoaded", function() {
-            new DataTable("#fixed-header", {
-                fixedHeader: !0
-            })
-        }), document.addEventListener("DOMContentLoaded", function() {
-            new DataTable("#model-datatables", {
-                responsive: {
-                    details: {
-                        display: $.fn.dataTable.Responsive.display.modal({
-                            header: function(e) {
-                                e = e.data();
-                                return "Details for " + e[0] + " " + e[1]
-                            }
-                        }),
-                        renderer: $.fn.dataTable.Responsive.renderer.tableAll({
-                            tableClass: "table"
-                        })
-                    }
-                }
-            })
-        }), document.addEventListener("DOMContentLoaded", function() {
-            new DataTable("#buttons-datatables", {
-                dom: "Bfrtip",
-                buttons: ["copy", "csv", "excel", "print", "pdf"]
-            })
-        }), document.addEventListener("DOMContentLoaded", function() {
-            new DataTable("#ajax-datatables", {
-                ajax: "assets/json/datatable.json"
             })
         });
     </script>
