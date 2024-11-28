17ccd8bc6b8e5904acc730bc4f156394ff8b6878
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-11-29 00:00:59 +0100

    User Managment Develop 33

--- app/Http/Controllers/Back/UserController.php
+++ app/Http/Controllers/Back/UserController.php
@@ -200,7 +200,7 @@ class UserController extends Controller
             $user = $object->data->users[0];
             if ($request->action == "edit") {
                 $body = '
-                <!--begin::Form-->
+                    <!--begin::Form-->
                     <form id="kt_modal_add_user_form" class="form" method="post" action="' . url('backend/user-update/' . $request->id) . '">
                         <input type="hidden" name="_token" value="' . csrf_token() . '">
 
@@ -266,32 +266,104 @@ class UserController extends Controller
                         <!--end::Actions-->
                     </form>
                     <!--end::Form-->
+
+                    <form action="' . url('backend/user-update/' . $request->id) . '" method="POST">
+                    <input type="hidden" name="_token" value="' . csrf_token() . '">
+
+                    <div class="ti-modal-header">
+                        <h3 class="ti-modal-title">
+                            Modifier un administrateur
+                        </h3>
+                        <button type="button" class="hs-dropdown-toggle ti-modal-clode-btn"
+                            data-hs-overlay="#hs-basic-modal">
+                            <span class="sr-only">Close</span>
+                            <svg class="w-3.5 h-3.5" width="8" height="8" viewBox="0 0 8 8" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M0.258206 1.00652C0.351976 0.912791 0.479126 0.860131 0.611706 0.860131C0.744296 0.860131 0.871447 0.912791 0.965207 1.00652L3.61171 3.65302L6.25822 1.00652C6.30432 0.958771 6.35952 0.920671 6.42052 0.894471C6.48152 0.868271 6.54712 0.854471 6.61352 0.853901C6.67992 0.853321 6.74572 0.865971 6.80722 0.891111C6.86862 0.916251 6.92442 0.953381 6.97142 1.00032C7.01832 1.04727 7.05552 1.1031 7.08062 1.16454C7.10572 1.22599 7.11842 1.29183 7.11782 1.35822C7.11722 1.42461 7.10342 1.49022 7.07722 1.55122C7.05102 1.61222 7.01292 1.6674 6.96522 1.71352L4.31871 4.36002L6.96522 7.00648C7.05632 7.10078 7.10672 7.22708 7.10552 7.35818C7.10442 7.48928 7.05182 7.61468 6.95912 7.70738C6.86642 7.80018 6.74102 7.85268 6.60992 7.85388C6.47882 7.85498 6.35252 7.80458 6.25822 7.71348L3.61171 5.06702L0.965207 7.71348C0.870907 7.80458 0.744606 7.85498 0.613506 7.85388C0.482406 7.85268 0.357007 7.80018 0.264297 7.70738C0.171597 7.61468 0.119017 7.48928 0.117877 7.35818C0.116737 7.22708 0.167126 7.10078 0.258206 7.00648L2.90471 4.36002L0.258206 1.71352C0.164476 1.61976 0.111816 1.4926 0.111816 1.36002C0.111816 1.22744 0.164476 1.10028 0.258206 1.00652Z"
+                                    fill="currentColor"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="ti-modal-body">
+
+                        <div class="mb-3">
+                            <label for="input-lastname" class="ti-form-label">Nom</label>
+                            <input type="text" name="last_name" id="input-lastname" class="ti-form-input" value="' . $user->last_name . '" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="input-firstname" class="ti-form-label">Prénom</label>
+                            <input type="text" name="first_name" id="input-firstname" value="' . $user->first_name . '" class="ti-form-input">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="input-email" class="ti-form-label">Email</label>
+                            <input type="email" name="email" id="input-email" class="ti-form-input" value="' . $user->email . '" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="input-phone" class="ti-form-label">Téléphone</label>
+                            <input type="tel" name="phone" id="phone" class="ti-form-input" value="' . $user->phone . '" required>
+                            <input id="phone_code" type="hidden" name="phone_code" value="' . $user->phone_code . '" />
+                        </div>
+
+                    </div>
+                    <div class="ti-modal-footer">
+                        <button type="button"
+                            class="hs-dropdown-toggle ti-btn ti-border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:ring-offset-white focus:ring-primary dark:bg-bgdark dark:hover:bg-black/20 dark:border-white/10 dark:text-white/70 dark:hover:text-white dark:focus:ring-offset-white/10"
+                            data-hs-overlay="#cardModalView">
+                            Annuler
+                        </button>
+                        <button class="ti-btn ti-btn-primary" type="submit">
+                            Valider
+                        </button>
+                    </div>
+                </form>
                     ';
             } elseif ($request->action == "delete") {
 
                 $body = '
                   <!--begin::Form-->
-                  <form id="kt_modal_add_permission_form" class="form" action="' . url('backend/user-update/' . $request->id) . '" method="POST">
-                      <input type="hidden" name="_token" value="' . csrf_token() . '">
-                      <input type="hidden" name="delete" value="true">
 
-                      <!--begin::Input group-->
-                      <div class="fv-row mb-7">
-                          <!--begin::Label-->
-                          <p>' . $Lang->trans('disclaimer_suppression_utilisateur') . '</p>
-                          <!--end::Label-->
-                      </div>
-                      <!--end::Input group-->
 
-                      <!--begin::Actions-->
-                      <div class="text-center pt-15">
-                          <button type="submit" class="btn btn-danger">
-                              <span class="indicator-label">Supprimer</span>
-                          </button>
-                      </div>
-                      <!--end::Actions-->
-                  </form>
-                  <!--end::Form-->';
+                  <div class="ti-modal-header">
+                        <h3 class="ti-modal-title">
+                        Supprimer un administrateur
+                        </h3>
+                        <button type="button" class="hs-dropdown-toggle ti-modal-clode-btn"
+                        data-hs-overlay="#hs-basic-modal">
+                        <span class="sr-only">Close</span>
+                        <svg class="w-3.5 h-3.5" width="8" height="8" viewBox="0 0 8 8" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                            d="M0.258206 1.00652C0.351976 0.912791 0.479126 0.860131 0.611706 0.860131C0.744296 0.860131 0.871447 0.912791 0.965207 1.00652L3.61171 3.65302L6.25822 1.00652C6.30432 0.958771 6.35952 0.920671 6.42052 0.894471C6.48152 0.868271 6.54712 0.854471 6.61352 0.853901C6.67992 0.853321 6.74572 0.865971 6.80722 0.891111C6.86862 0.916251 6.92442 0.953381 6.97142 1.00032C7.01832 1.04727 7.05552 1.1031 7.08062 1.16454C7.10572 1.22599 7.11842 1.29183 7.11782 1.35822C7.11722 1.42461 7.10342 1.49022 7.07722 1.55122C7.05102 1.61222 7.01292 1.6674 6.96522 1.71352L4.31871 4.36002L6.96522 7.00648C7.05632 7.10078 7.10672 7.22708 7.10552 7.35818C7.10442 7.48928 7.05182 7.61468 6.95912 7.70738C6.86642 7.80018 6.74102 7.85268 6.60992 7.85388C6.47882 7.85498 6.35252 7.80458 6.25822 7.71348L3.61171 5.06702L0.965207 7.71348C0.870907 7.80458 0.744606 7.85498 0.613506 7.85388C0.482406 7.85268 0.357007 7.80018 0.264297 7.70738C0.171597 7.61468 0.119017 7.48928 0.117877 7.35818C0.116737 7.22708 0.167126 7.10078 0.258206 7.00648L2.90471 4.36002L0.258206 1.71352C0.164476 1.61976 0.111816 1.4926 0.111816 1.36002C0.111816 1.22744 0.164476 1.10028 0.258206 1.00652Z"
+                            fill="currentColor" />
+                        </svg>
+                        </button>
+                    </div>
+                    <div class="ti-modal-body">
+                        <p class="mt-1 text-gray-800 dark:text-white/70">
+                        Êtes-vous sûr de vouloir supprimer cet administrateur ?
+                        </p>
+                    </div>
+                    <div class="ti-modal-footer">
+                        <form id="kt_modal_add_permission_form" class="form" action="' . url('backend/user-update/' . $request->id) . '" method="POST">
+                            <input type="hidden" name="_token" value="' . csrf_token() . '">
+                            <input type="hidden" name="delete" value="true">
+
+                            <button type="button"
+                            class="hs-dropdown-toggle ti-btn ti-border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:ring-offset-white focus:ring-primary dark:bg-bgdark dark:hover:bg-black/20 dark:border-white/10 dark:text-white/70 dark:hover:text-white dark:focus:ring-offset-white/10"
+                            data-hs-overlay="#cardModalView">
+                            Fermer
+                            </button>
+                            <button class="ti-btn ti-btn-danger" type="submit">
+                            Supprimer
+                            </button>
+                        </form>
+                        <!--end::Form-->
+                    </div>
+                  ';
             }
 
             $response = array(
