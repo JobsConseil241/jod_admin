29e4d45e77508a093084a2988ee1636b9ff62882
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-11-29 00:04:59 +0100

    User Managment Develop 35

--- resources/views/back/user/administrator.blade.php
+++ resources/views/back/user/administrator.blade.php
@@ -189,6 +189,9 @@
                 language: {
                     url: "https://cdn.datatables.net/plug-ins/1.10.25/i18n/French.json"
                 },
+                order: [
+                    [0, 'desc']
+                ],
                 searching: true
             })
         });
