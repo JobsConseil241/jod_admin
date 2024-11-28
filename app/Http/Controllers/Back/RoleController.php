5ed89f8764dfcfc6ccc478978690d09a69b2f805
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-11-28 22:32:49 +0100

    User Managment Develop 21

--- app/Http/Controllers/Back/RoleController.php
+++ app/Http/Controllers/Back/RoleController.php
@@ -77,7 +77,7 @@ class RoleController extends Controller
         $response = Http::withHeaders([
             "Authorization" => "Bearer " . $access_token
         ])->get(env('SERVER_PC') . 'get_roles');
-        dd($response);
+
         $object = json_decode($response->body());
 
         if ($object && $object->success == true) {
