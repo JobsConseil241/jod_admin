c780dc18678b191fb9c16cff5a674db86ea96bea
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-12-09 14:39:27 +0100

    Add Marque Car 4

--- app/Http/Controllers/Back/CarController.php
+++ app/Http/Controllers/Back/CarController.php
@@ -263,6 +263,8 @@ class CarController extends Controller
 
         $object = json_decode($response->body());
 
+        dd($response);
+
         if ($object && $object->success == true) {
             return back()->with('success', "la marque a été créé avec succès.");
         } else {
@@ -283,8 +285,7 @@ class CarController extends Controller
             ]);
 
             $object = json_decode($response->body());
-            dd($response);
-            die;
+
             if ($object && $object->success == true) {
                 return back()->with('success', "la marque a été supprimé avec succès.");
             } else {
