207567ba0681bcb8c0bb4318524ae15ecc809fd2
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-12-31 00:33:48 +0100

    Fix update picture 6

--- resources/views/back/car/picture.blade.php
+++ resources/views/back/car/picture.blade.php
@@ -62,12 +62,12 @@
                               file:py-2 file:px-4
                               dark:file:bg-black/20 dark:file:text-white/70"
                                                         name="{{ $field }}" accept="image/*" data-max-file-size="2MB"
-                                                        data-current-url="{{ $car->vehicule_media[0]->{$field} ? asset($car->vehicule_media[0]->{$field}) : '' }}">
+                                                        data-current-url="{{ $car->vehicule_medias[0]->{$field} ? asset($car->vehicule_medias[0]->{$field}) : '' }}">
 
                                                     <!-- Conteneur de prévisualisation -->
                                                     <div id="preview-{{ $field }}" class="preview-container mt-3">
-                                                        @if ($car->vehicule_media[0]->{$field})
-                                                            <img src="{{ asset($car->vehicule_media[0]->{$field}) }}"
+                                                        @if ($car->vehicule_medias[0]->{$field})
+                                                            <img src="{{ asset($car->vehicule_medias[0]->{$field}) }}"
                                                                 alt="{{ $label }}" class="max-h-32 rounded shadow">
                                                         @endif
                                                     </div>
