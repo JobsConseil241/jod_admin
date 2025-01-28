0a0970c271afa2cf1b1667f83400acbe126dbc95
JobsConseil241 <jobsconseil241@gmail.com>
2025-01-28 21:24:09 +0100

    update home page

--- app/Http/Controllers/Back/CarController.php
+++ app/Http/Controllers/Back/CarController.php
@@ -169,6 +169,8 @@ class CarController extends Controller
             $categories = [];
         }
 
+        dd($categorie);
+
         return view('back.car.car_panne', compact('car', 'categories'));
     }
 
