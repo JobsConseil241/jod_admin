c2e37af721dffbac6419f61d6f8006429321c85e
JobsConseil241 <jobsconseil241@gmail.com>
2025-02-19 11:41:06 +0100

    car detail bug solving

--- app/Http/Controllers/Front/CarsController.php
+++ app/Http/Controllers/Front/CarsController.php
@@ -19,7 +19,7 @@ class CarsController extends Controller
     {
         $car = Vehicule::with(['categorie', 'marque', 'vehiculeMedias', 'latestEtat'])->where('name', $name)->first();
 
-        dd($car->vehiculeMedias->photo_arriere);
+        dd($car->vehiculeMedias);
         return view('front.car.item', compact('car'));
     }
 }
