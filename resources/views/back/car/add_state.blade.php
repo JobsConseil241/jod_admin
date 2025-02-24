9224ac1938f4a131c4d9ff9aa777eb1629fce7b3
JobsConseil241 <jobsconseil241@gmail.com>
2025-02-24 11:38:44 +0100

    car etat bug solving

--- resources/views/back/car/add_state.blade.php
+++ resources/views/back/car/add_state.blade.php
@@ -103,7 +103,6 @@
                                                         <input type="date" id="input-{{ $field }}"
                                                             name="{{ $field }}"
                                                             oninput="this.value = this.value || new Date().toISOString().split('T')[0]"
-                                                            value="{{ old($field) }}"
                                                             class="block w-full border border-gray-200 focus:shadow-sm dark:focus:shadow-white/10 rounded-sm text-sm focus:z-10 focus:outline-0 focus:border-gray-200 dark:focus:border-white/10 dark:border-white/10 dark:text-white/70">
                                                     @endif
                                                 </div>
