2cfc91d4f6ea6cbca4f4564ccbf3421410ebf587
JobsConseil241 <jobsconseil241@gmail.com>
2025-02-27 02:44:28 +0100

    car reservation  bug solving

--- resources/views/back/booking/add.blade.php
+++ resources/views/back/booking/add.blade.php
@@ -336,6 +336,7 @@
 @endsection
 
 @push('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!-- Quill Editor JS -->
     <script src="{{ asset('back/libs/quill/quill.min.js') }}"></script>
 
