8bfcfd14d374dd629c64e399d69d1cb38b11cc71
JobsConseil241 <jobsconseil241@gmail.com>
2025-02-27 01:01:12 +0100

    car reservation  bug solving

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -628,7 +628,7 @@
 
     </div>
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!-- Apex Charts JS -->
 {{--    <script src="{{ asset('back/libs/apexcharts/apexcharts.min.js') }}"></script>--}}
 
