28883dc4ea220f83e407360498a296c5fbca5557
JobsConseil241 <jobsconseil241@gmail.com>
2025-03-13 12:51:32 +0100

    car reservation & paiement  bug solving

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule as ScheduleFacade;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Votre tâche de détection des retards de paiement
+ScheduleFacade::command('recouvrements:detecter-retards')->everyMinute()->sendOutputTo('recouvrement-output.log');
