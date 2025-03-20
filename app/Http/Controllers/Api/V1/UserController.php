483d37cc71b4e7ab7d63e94b0c851bcf774d7f69
JobsConseil241 <jobsconseil241@gmail.com>
2025-03-20 16:35:13 +0100

    car dashboard bug solving

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -178,7 +178,7 @@ class UserController extends BaseController
 
             $query = User::with(['userType', 'roles', 'roles.privileges']);
             $query = $query->where('deleted', NULL);
-            $query->where('roles.pivot.role_id', 1000002);
+//            $query->where('roles.pivot.role_id', 1000002);
 
             $data['count'] = $query->count();
             $pagination = $query
