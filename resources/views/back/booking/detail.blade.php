e98ad9dc5f57db59fae5892f6633f2b6477c6a5b
JobsConseil241 <jobsconseil241@gmail.com>
2025-04-03 12:15:43 +0100

    welcome page update view

--- resources/views/back/booking/detail.blade.php
+++ resources/views/back/booking/detail.blade.php
@@ -920,5 +920,12 @@
                 });
             }
         });
+
+        function formatDate(date) {
+            var day = ('0' + date.getDate()).slice(-2);
+            var month = ('0' + (date.getMonth() + 1)).slice(-2);
+            var year = date.getFullYear();
+            return day + '/' + month + '/' + year;
+        }
     </script>
 @endpush
