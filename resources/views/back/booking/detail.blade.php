fd9cd68d93985cb025e1e441e49be945ecd158f6
JobsConseil241 <jobsconseil241@gmail.com>
2025-04-03 11:59:07 +0100

    welcome page update view

--- resources/views/back/booking/detail.blade.php
+++ resources/views/back/booking/detail.blade.php
@@ -759,6 +759,52 @@
                         </div>
                     </div>
                 </div>
+
+                <div id="hs-vehicle-state-modal" class="hs-overlay ti-modal hidden">
+                    <div class="ti-modal-box">
+                        <div class="ti-modal-content">
+                            <form action="{{ route('backend.booking.assign.pannes', ['reference' => $reference]) }}" method="post">
+                                @csrf
+                                <div class="ti-modal-header">
+                                    <h3 class="ti-modal-title">
+                                        Ajouter l'etat final du vehicule
+                                    </h3>
+                                    <button type="button" class="hs-dropdown-toggle ti-modal-clode-btn"
+                                            data-hs-overlay="#hs-basic-modal">
+                                        <span class="sr-only">Close</span>
+                                        <svg class="w-3.5 h-3.5" width="8" height="8" viewBox="0 0 8 8" fill="none"
+                                             xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M0.258206 1.00652C0.351976 0.912791 0.479126 0.860131 0.611706 0.860131C0.744296 0.860131 0.871447 0.912791 0.965207 1.00652L3.61171 3.65302L6.25822 1.00652C6.30432 0.958771 6.35952 0.920671 6.42052 0.894471C6.48152 0.868271 6.54712 0.854471 6.61352 0.853901C6.67992 0.853321 6.74572 0.865971 6.80722 0.891111C6.86862 0.916251 6.92442 0.953381 6.97142 1.00032C7.01832 1.04727 7.05552 1.1031 7.08062 1.16454C7.10572 1.22599 7.11842 1.29183 7.11782 1.35822C7.11722 1.42461 7.10342 1.49022 7.07722 1.55122C7.05102 1.61222 7.01292 1.6674 6.96522 1.71352L4.31871 4.36002L6.96522 7.00648C7.05632 7.10078 7.10672 7.22708 7.10552 7.35818C7.10442 7.48928 7.05182 7.61468 6.95912 7.70738C6.86642 7.80018 6.74102 7.85268 6.60992 7.85388C6.47882 7.85498 6.35252 7.80458 6.25822 7.71348L3.61171 5.06702L0.965207 7.71348C0.870907 7.80458 0.744606 7.85498 0.613506 7.85388C0.482406 7.85268 0.357007 7.80018 0.264297 7.70738C0.171597 7.61468 0.119017 7.48928 0.117877 7.35818C0.116737 7.22708 0.167126 7.10078 0.258206 7.00648L2.90471 4.36002L0.258206 1.71352C0.164476 1.61976 0.111816 1.4926 0.111816 1.36002C0.111816 1.22744 0.164476 1.10028 0.258206 1.00652Z"
+                                                fill="currentColor"></path>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="ti-modal-body">
+
+                                    <div class="mb-3">
+                                        <label class="ti-form-label mb-0">Etats du vehicule ({{ $booking->name }})</label>
+                                        <select name="id_etat_vehicule" class="ti-form-select" required>
+                                            <option selected readonly>--- Choisissez l'état associe ---</option>
+                                        </select>
+                                        <input type="hidden" name="id_location" value="{{ $reference }}">
+                                        <p class="text-sm text-gray-500 mt-2 dark:text-white/70 tett" id="hs-input-helper-text">Ajouter un etat si pas disponible</p>
+                                    </div>
+                                </div>
+                                <div class="ti-modal-footer">
+                                    <button type="button"
+                                            class="hs-dropdown-toggle ti-btn ti-border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:ring-offset-white focus:ring-primary dark:bg-bgdark dark:hover:bg-black/20 dark:border-white/10 dark:text-white/70 dark:hover:text-white dark:focus:ring-offset-white/10"
+                                            data-hs-overlay="#hs-basic-modal">
+                                        Annuler
+                                    </button>
+                                    <button class="ti-btn ti-btn-primary" type="submit">
+                                        Enregistrer
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -801,6 +847,8 @@
     <script>
         "use strict";
 
+        let location = @json($booking)
+
         $(document).on("click", ".delete_action", function() {
             var id = $(this).data('id');
             $.ajax({
@@ -822,6 +870,51 @@
                     $('#cardModalView').removeClass('hidden').addClass('open');
                 }
             });
+        });
+
+        $(document).ready(function() {
+            $('[data-hs-overlay="#hs-vehicle-state-modal"]').on('click', function() {
+                $('#hs-vehicle-state-modal').modal('show');
+
+                fetchPannes();
+            });
+
+            function fetchPannes() {
+                $.ajax({
+                    url: '/public/backend/booking/car/pannes/' + location.vehicule.id + '/ajax',
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function(data) {
+
+                        $('#id_etat_vehicule').append('<option value="">Sélectionnez un etat</option>');
+
+                        $.each(data, function(key, panne) {
+                            var dateObj = new Date(panne.date);
+                            var formattedDate = formatDate(dateObj);
+
+                            $('#id_etat_vehicule').append('<option value="' + panne.id + '"> Etat du ' + formattedDate + '</option>');
+                        });
+
+                        $('.tett').each(function(index) {
+                            // Créer un lien
+                            var lien = $('<a>', {
+                                href: '/public/backend/car/etat/' + location.vehicule.id,
+                                text: 'ICI',
+                                class: 'lien-ajouter-etat text-success',
+                                'data-index': index
+                            });
+
+                            $(this).empty().append(
+                                'Ajouter un état si pas disponible ',
+                                lien
+                            );
+                        });
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Erreur lors du chargement des pannes:', error);
+                    }
+                });
+            }
         });
     </script>
 @endpush
